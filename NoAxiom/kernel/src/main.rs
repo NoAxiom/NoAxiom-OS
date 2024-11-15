0cf8f9b9b243ba7f59db29d8dd012589757e3d87
crpboy <crpboy2004@gmail.com>
2024-11-15 18:49:33 +0800

    config: allow unused code

--- NoAxiom/kernel/src/main.rs
+++ NoAxiom/kernel/src/main.rs
@@ -8,10 +8,10 @@
 #![feature(panic_info_message)]
 #![feature(alloc_error_handler)]
 #![feature(slice_from_ptr_range)]
+#![allow(dead_code, unused_imports, unused_variables)]
 // #![feature(error_in_core)]
 // #![feature(negative_impls)]
 // #![feature(ascii_char)]
-// #![allow(dead_code, unused_imports, unused_variables)]
 // #![feature(custom_mir)]
 // #![feature(core_intrinsics)]
 
