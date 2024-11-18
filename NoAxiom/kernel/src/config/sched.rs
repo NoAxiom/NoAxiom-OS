32c340ea62a1954a331eae1538e37ec4961a6ab7
crpboy <crpboy2004@gmail.com>
2024-11-18 17:38:40 +0800

    feat(MLFQ): add prio levels

--- NoAxiom/kernel/src/config/sched.rs
+++ NoAxiom/kernel/src/config/sched.rs
@@ -1,1 +1,3 @@
 //! task schedule configs
+
+pub const MLFQ_LEVELS: usize = 32;
--- NoAxiom/kernel/src/sched/executor.rs
+++ NoAxiom/kernel/src/sched/executor.rs
@@ -2,14 +2,14 @@
 //! - [`spawn_raw`] to add a task
 //! - [`run`] to run next task
 
-use alloc::collections::vec_deque::VecDeque;
+use alloc::{collections::vec_deque::VecDeque, vec::Vec};
 use core::future::Future;
 
 use async_task::{Builder, Runnable, ScheduleInfo, WithInfo};
 use lazy_static::lazy_static;
 
 use super::utask::TaskFuture;
-use crate::sync::mutex::SpinMutex;
+use crate::{config::sched::MLFQ_LEVELS, sync::mutex::SpinMutex};
 
 struct TaskInfo {
     prio: usize,
@@ -19,39 +19,62 @@ impl TaskInfo {
     pub const fn new(prio: usize) -> Self {
         Self { prio }
     }
+    pub fn level(&self) -> usize {
+        self.prio
+    }
 }
 
 pub(crate) struct Executor {
-    queue: SpinMutex<VecDeque<Runnable<TaskInfo>>>,
+    queue: Vec<VecDeque<Runnable<TaskInfo>>>,
+    begin_level: usize, // the level to start searching
 }
 
 impl Executor {
-    pub const fn new() -> Self {
+    pub fn new() -> Self {
+        let mut vec = Vec::new();
+        for _ in 0..MLFQ_LEVELS {
+            vec.push(VecDeque::new());
+        }
         Self {
-            queue: SpinMutex::new(VecDeque::new()),
+            queue: vec,
+            begin_level: 0,
         }
     }
-    fn push_back(&self, runnable: Runnable<TaskInfo>) {
-        self.queue.lock().push_back(runnable);
+    fn push_back(&mut self, level: usize, runnable: Runnable<TaskInfo>) {
+        self.begin_level = self.begin_level.min(level);
+        self.queue[level].push_back(runnable);
     }
-    fn push_front(&self, runnable: Runnable<TaskInfo>) {
-        self.queue.lock().push_front(runnable);
+    fn push_front(&mut self, level: usize, runnable: Runnable<TaskInfo>) {
+        self.begin_level = self.begin_level.min(level);
+        self.queue[level].push_front(runnable);
     }
-    fn pop_front(&self) -> Option<Runnable<TaskInfo>> {
-        self.queue.lock().pop_front()
+    fn pop_front(&mut self) -> Option<Runnable<TaskInfo>> {
+        for i in self.begin_level..MLFQ_LEVELS {
+            let info = self.queue[i].pop_front();
+            if info.is_some() {
+                self.begin_level = i;
+                return info;
+            }
+        }
+        self.begin_level = MLFQ_LEVELS;
+        None
+    }
+    #[inline(always)]
+    fn update_begin_level(&mut self, level: usize) {
+        self.begin_level = level;
     }
 }
 
 lazy_static! {
-    static ref EXECUTOR: Executor = Executor::new();
+    static ref EXECUTOR: SpinMutex<Executor> = SpinMutex::new(Executor::new());
 }
 
 fn schedule(task: Runnable<TaskInfo>, info: ScheduleInfo) {
-    let prio = task.metadata();
+    let level = task.metadata().level();
     if info.woken_while_running {
-        EXECUTOR.push_front(task);
+        EXECUTOR.lock().push_front(level, task);
     } else {
-        EXECUTOR.push_back(task);
+        EXECUTOR.lock().push_back(level, task);
     }
 }
 
@@ -74,8 +97,7 @@ where
 pub fn run() {
     // spin until find a valid task
     loop {
-        if let Some(task) = EXECUTOR.pop_front() {
-            task.metadata();
+        if let Some(task) = EXECUTOR.lock().pop_front() {
             task.run();
             break;
         }
