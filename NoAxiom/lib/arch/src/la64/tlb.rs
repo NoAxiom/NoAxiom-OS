7b5448d93e7697165e748e67277b7e6c02bb1a12
crpboy <crpboy2004@gmail.com>
2025-04-02 23:00:00 +0800

    refactor(tlb): move bare asm from tlb.rs to tlb.S

--- NoAxiom/lib/arch/src/la64/tlb.rs
+++ NoAxiom/lib/arch/src/la64/tlb.rs
@@ -1,80 +1,11 @@
-use core::arch::asm;
+use core::arch::global_asm;
 
 use config::mm::PAGE_WIDTH;
 use loongArch64::register::{pwch, pwcl, stlbps, tlbidx, tlbrehi, tlbrentry::set_tlbrentry};
 
-#[naked]
-pub unsafe extern "C" fn tlb_refill() {
-    asm!(
-        // PGD: 0x1b CRMD:0x0 PWCL:0x1c TLBRBADV:0x89 TLBERA:0x8a TLBRSAVE:0x8b SAVE:0x30
-        // TLBREHi: 0x8e STLBPS: 0x1e MERRsave:0x95
-        "
-        .equ LA_CSR_TLBEHI,        0x11    /* TLB entry high */
-        .equ LA_CSR_TLBELO0,       0x12    /* TLB entry low 0 */
-        .equ LA_CSR_TLBELO1,       0x13    /* TLB entry low 1 */
-        .equ LA_CSR_PGDL,          0x19    /* Page table base address when VA[47] = 0 */
-        .equ LA_CSR_PGDH,          0x1a    /* Page table base address when VA[47] = 1 */
-        .equ LA_CSR_PGD,           0x1b    /* Page table base */
-        .equ LA_CSR_TLBRENTRY,     0x88    /* TLB refill exception entry */
-        .equ LA_CSR_TLBRBADV,      0x89    /* TLB refill badvaddr */
-        .equ LA_CSR_TLBRERA,       0x8a    /* TLB refill ERA */
-        .equ LA_CSR_TLBRSAVE,      0x8b    /* KScratch for TLB refill exception */
-        .equ LA_CSR_TLBRELO0,      0x8c    /* TLB refill entrylo0 */
-        .equ LA_CSR_TLBRELO1,      0x8d    /* TLB refill entrylo1 */
-        .equ LA_CSR_TLBREHI,       0x8e    /* TLB refill entryhi */
-        .balign 4096
-            csrwr  $t0, LA_CSR_TLBRSAVE
-
-            csrrd  $t0, LA_CSR_PGD
-            lddir  $t0, $t0, 3
-            andi   $t0, $t0, 1
-            beqz   $t0, 1f
-
-            csrrd  $t0, LA_CSR_PGD
-            lddir  $t0, $t0, 3
-            addi.d $t0, $t0, -1
-            lddir  $t0, $t0, 1
-            andi   $t0, $t0, 1
-            beqz   $t0, 1f
-            csrrd  $t0, LA_CSR_PGD
-            lddir  $t0, $t0, 3
-            addi.d $t0, $t0, -1
-            lddir  $t0, $t0, 1
-            addi.d $t0, $t0, -1
-
-            ldpte  $t0, 0
-            ldpte  $t0, 1
-            csrrd  $t0, LA_CSR_TLBRELO0
-            csrrd  $t0, LA_CSR_TLBRELO1
-            csrrd  $t0, 0x0
-        2:
-            tlbfill
-            csrrd  $t0, LA_CSR_TLBRBADV
-            srli.d $t0, $t0, 13
-            slli.d $t0, $t0, 13
-            csrwr  $t0, LA_CSR_TLBEHI
-            tlbsrch
-            tlbrd
-            csrrd  $t0, LA_CSR_TLBELO0
-            csrrd  $t0, LA_CSR_TLBELO1
-            csrrd  $t0, LA_CSR_TLBRSAVE
-            ertn
-        1:
-            csrrd  $t0, LA_CSR_TLBREHI
-            ori    $t0, $t0, 0xC
-            csrwr  $t0, LA_CSR_TLBREHI
-
-            rotri.d $t0, $t0, 61
-            ori    $t0, $t0, 3
-            rotri.d $t0, $t0, 3
-
-            csrwr  $t0, LA_CSR_TLBRELO0
-            csrrd  $t0, LA_CSR_TLBRELO0
-            csrwr  $t0, LA_CSR_TLBRELO1
-            b      2b
-        ",
-        options(noreturn)
-    )
+global_asm!(include_str!("./tlb.S"));
+extern "C" {
+    fn __tlb_refill();
 }
 
 #[inline]
@@ -95,7 +26,7 @@ pub fn flush_vaddr(va: usize) {
 #[inline]
 pub fn tlb_flush_all() {
     unsafe {
-        core::arch::asm!("invtlb 0x0,$zero, $zero");
+        core::arch::asm!("invtlb 0x0, $zero, $zero");
     }
 }
 
@@ -121,5 +52,5 @@ pub fn tlb_init_inner() {
     pwch::set_dir3_base(PAGE_WIDTH + DIR_WIDTH * 2);
     pwch::set_dir3_width(DIR_WIDTH);
 
-    set_tlb_refill_entry(tlb_refill as usize);
+    set_tlb_refill_entry(__tlb_refill as usize);
 }
