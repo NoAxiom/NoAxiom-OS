a58eceeda7ad4457b083b503533e560ffa64dc6e
crpboy <crpboy2004@gmail.com>
2025-05-10 16:19:09 +0800

    perf(wait): revert to previous wait impls

--- NoAxiom/kernel/src/task/wait.rs
+++ NoAxiom/kernel/src/task/wait.rs
@@ -1,3 +1,129 @@
+use alloc::sync::Arc;
+use core::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use super::{status::TaskStatus, Task};
+use crate::{
+    include::{
+        process::{PidSel, WaitOption},
+        result::Errno,
+    },
+    sched::utils::{after_suspend, before_suspend},
+    signal::sig_set::SigSet,
+    syscall::SysResult,
+};
+
+pub struct WaitChildFuture<'a> {
+    task: &'a Arc<Task>,
+    target: Option<Arc<Task>>,
+    wait_option: WaitOption,
+}
+
+impl<'a> WaitChildFuture<'a> {
+    pub fn new(task: &'a Arc<Task>, pid_type: PidSel, wait_option: WaitOption) -> SysResult<Self> {
+        let pcb = task.pcb();
+        if pcb.children.is_empty() && pcb.zombie_children.is_empty() {
+            return Err(Errno::ECHILD);
+        }
+        let target = match pid_type {
+            PidSel::Task(None) => None,
+            PidSel::Task(Some(tgid)) => match pcb.children.iter().find(|child| child.tid() == tgid)
+            {
+                Some(child) => Some(child.clone()),
+                None => return Err(Errno::ECHILD),
+            },
+            PidSel::Group(_) => return Err(Errno::EINVAL),
+        };
+        debug!("wait for child: {:?}", target.as_ref().map(|x| x.tid()));
+        drop(pcb);
+        Ok(Self {
+            task,
+            target,
+            wait_option,
+        })
+    }
+}
+
+impl Future for WaitChildFuture<'_> {
+    type Output = SysResult<(i32, usize)>;
+    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut pcb = self.task.pcb();
+        let res = match &self.target {
+            None => match pcb.zombie_children.pop() {
+                Some(child) => {
+                    // time statistic
+                    self.task
+                        .tcb_mut()
+                        .time_stat
+                        .add_child_time(child.tcb().time_stat.child_time());
+                    Poll::Ready(Ok((pcb.exit_code(), child.tid())))
+                }
+                None => Poll::Pending,
+            },
+            Some(child) => {
+                let mut ch_pcb = child.pcb();
+                match ch_pcb.status() {
+                    TaskStatus::Zombie => {
+                        let target_tid = child.tid();
+                        let exit_code = ch_pcb.exit_code();
+                        // since we already collected exit info
+                        // so just delete it from zombie children
+                        ch_pcb
+                            .zombie_children
+                            .retain(|task| task.tid() != target_tid);
+                        // update time statistic
+                        self.task
+                            .tcb_mut()
+                            .time_stat
+                            .add_child_time(child.tcb().time_stat.child_time());
+                        Poll::Ready(Ok((exit_code, target_tid)))
+                    }
+                    _ => Poll::Pending,
+                }
+            }
+        };
+        let res = match res {
+            Poll::Pending => {
+                if self.wait_option.contains(WaitOption::WNOHANG) && res.is_pending() {
+                    trace!("[sys_wait4] return nohang");
+                    Poll::Ready(Ok((0, 0)))
+                } else {
+                    trace!("[sys_wait4] suspend for child exit");
+                    Poll::Pending
+                }
+            }
+            Poll::Ready(_) => {
+                trace!("[sys_wait4] exited child found");
+                res
+            }
+        };
+        match res {
+            Poll::Pending => {
+                before_suspend(pcb, Some(SigSet::SIGCHLD));
+            }
+            Poll::Ready(_) => {
+                after_suspend(Some(pcb));
+            }
+        }
+        res
+    }
+}
+
+impl Task {
+    pub async fn wait_child(
+        self: &Arc<Self>,
+        pid_type: PidSel,
+        wait_option: WaitOption,
+    ) -> SysResult<(i32, usize)> {
+        WaitChildFuture::new(self, pid_type, wait_option)?.await
+    }
+}
+
+/*
+
 use alloc::{sync::Arc, vec::Vec};
 use core::{
     future::Future,
@@ -128,121 +254,6 @@ impl Task {
             wait_option,
         }
         .await
-    }
-}
-
-/*
-
-use alloc::sync::Arc;
-use core::{
-    future::Future,
-    pin::Pin,
-    task::{Context, Poll},
-};
-
-use super::{status::TaskStatus, Task};
-use crate::{
-    include::{
-        process::{PidSel, WaitOption},
-        result::Errno,
-    },
-    sched::utils::{after_suspend, before_suspend},
-    signal::sig_set::SigSet,
-    syscall::SysResult,
-};
-
-pub struct WaitChildFuture {
-    task: Arc<Task>,
-    target: Option<Arc<Task>>,
-    wait_option: WaitOption,
-}
-
-impl WaitChildFuture {
-    pub fn new(task: Arc<Task>, pid_type: PidSel, wait_option: WaitOption) -> SysResult<Self> {
-        let pcb = task.pcb();
-        if pcb.children.is_empty() && pcb.zombie_children.is_empty() {
-            return Err(Errno::ECHILD);
-        }
-        let target = match pid_type {
-            PidSel::Task(None) => None,
-            PidSel::Task(Some(tgid)) => match pcb.children.iter().find(|task| task.tid() == tgid) {
-                Some(task) => Some(task.clone()),
-                None => return Err(Errno::ECHILD),
-            },
-            PidSel::Group(_) => return Err(Errno::EINVAL),
-        };
-        debug!("wait for child: {:?}", target.as_ref().map(|x| x.tid()));
-        drop(pcb);
-        Ok(Self {
-            task,
-            target,
-            wait_option,
-        })
-    }
-}
-
-impl Future for WaitChildFuture {
-    type Output = SysResult<(i32, usize)>;
-    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut pcb = self.task.pcb();
-        let res = match &self.target {
-            None => match pcb.zombie_children.pop() {
-                Some(child) => {
-                    // time statistic
-                    self.task
-                        .tcb_mut()
-                        .time_stat
-                        .add_child_time(child.tcb().time_stat.child_time());
-                    Poll::Ready(Ok((pcb.exit_code(), child.tid())))
-                }
-                None => Poll::Pending,
-            },
-            Some(child) => {
-                let mut ch_pcb = child.pcb();
-                match ch_pcb.status() {
-                    TaskStatus::Zombie => {
-                        let target_tid = child.tid();
-                        let exit_code = ch_pcb.exit_code();
-                        // since we already collected exit info
-                        // so just delete it from zombie children
-                        ch_pcb
-                            .zombie_children
-                            .retain(|task| task.tid() != target_tid);
-                        // update time statistic
-                        self.task
-                            .tcb_mut()
-                            .time_stat
-                            .add_child_time(child.tcb().time_stat.child_time());
-                        Poll::Ready(Ok((exit_code, target_tid)))
-                    }
-                    _ => Poll::Pending,
-                }
-            }
-        };
-        let res = match res {
-            Poll::Pending => {
-                if self.wait_option.contains(WaitOption::WNOHANG) && res.is_pending() {
-                    trace!("[sys_wait4] return nohang");
-                    Poll::Ready(Ok((0, 0)))
-                } else {
-                    trace!("[sys_wait4] suspend for child exit");
-                    Poll::Pending
-                }
-            }
-            Poll::Ready(_) => {
-                trace!("[sys_wait4] exited child found");
-                res
-            }
-        };
-        match res {
-            Poll::Pending => {
-                before_suspend(pcb, Some(SigSet::SIGCHLD));
-            }
-            Poll::Ready(_) => {
-                after_suspend(Some(pcb));
-            }
-        }
-        res
     }
 }
 
