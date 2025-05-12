dcb75751a4e295a22f99f2965df4edf653c96dc4
crpboy <crpboy2004@gmail.com>
2025-05-12 14:41:43 +0800

    refactor(init): move task runner to runtime.rs

--- NoAxiom/kernel/src/sched/runtime.rs
+++ NoAxiom/kernel/src/sched/runtime.rs
@@ -1,18 +1,27 @@
 use lazy_static::lazy_static;
 
+use super::{
+    simple::{SimpleRuntime, SimpleScheduler},
+    vsched::Runtime,
+};
+use crate::{cpu::get_hartid, time::sleep::timer_handler};
+
+lazy_static! {
+    pub static ref RUNTIME: SimpleRuntime<SimpleScheduler> = SimpleRuntime::new();
+}
+
 // use super::cfs::CfsRuntime;
 // use crate::sched::vsched::Runtime;
 // lazy_static! {
 //     pub(crate) static ref RUNTIME: CfsRuntime = CfsRuntime::new();
 // }
-use super::{
-    simple::{SimpleRuntime, SimpleScheduler},
-    vsched::Runtime,
-};
-lazy_static! {
-    pub static ref RUNTIME: SimpleRuntime<SimpleScheduler> = SimpleRuntime::new();
-}
 
-pub fn run() {
-    RUNTIME.run();
+/// run_tasks: only act as a task runner
+#[no_mangle]
+pub fn run_tasks() -> ! {
+    info!("[kernel] hart {} has been booted", get_hartid());
+    loop {
+        timer_handler();
+        RUNTIME.run();
+    }
 }
