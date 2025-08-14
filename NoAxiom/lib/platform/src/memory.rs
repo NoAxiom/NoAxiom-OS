5cc09e7237ae3ede7140883dcad701fc5ec177d0
crpboy <crpboy2004@gmail.com>
2025-08-15 05:07:03 +0800

    chore(mm): expand heap size to avoid panic

--- NoAxiom/lib/platform/src/memory.rs
+++ NoAxiom/lib/platform/src/memory.rs
@@ -4,7 +4,7 @@ mod config {
     mod config {
         const PHYS_MEMORY_START: usize = 0x8000_0000;
         const MEMORY_SIZE: usize = 0x4000_0000;
-        pub const KERNEL_HEAP_SIZE: usize = 0x1000_0000;
+        pub const KERNEL_HEAP_SIZE: usize = 0x1200_0000;
         pub const PHYS_MEMORY_END: usize = PHYS_MEMORY_START + MEMORY_SIZE;
         pub const VALID_PHYS_CPU_MASK: usize = 0b1111;
     }
@@ -12,7 +12,7 @@ mod config {
     mod config {
         const PHYS_MEMORY_START: usize = 0x4000_0000;
         const MEMORY_SIZE: usize = 0x1_0000_0000;
-        pub const KERNEL_HEAP_SIZE: usize = 0x1000_0000;
+        pub const KERNEL_HEAP_SIZE: usize = 0x1200_0000;
         pub const PHYS_MEMORY_END: usize = PHYS_MEMORY_START + MEMORY_SIZE;
         pub const VALID_PHYS_CPU_MASK: usize = 0b11110;
     }
@@ -25,7 +25,7 @@ mod config {
     mod config {
         const PHYS_MEMORY_START: usize = 0x9000_0000;
         const MEMORY_SIZE: usize = 0x2000_0000;
-        pub const KERNEL_HEAP_SIZE: usize = 0x800_0000;
+        pub const KERNEL_HEAP_SIZE: usize = 0x1200_0000;
         pub const PHYS_MEMORY_END: usize = PHYS_MEMORY_START + MEMORY_SIZE;
         pub const VALID_PHYS_CPU_MASK: usize = 0b1111;
     }
@@ -33,7 +33,7 @@ mod config {
     mod config {
         const PHYS_MEMORY_START: usize = 0x9000_0000;
         const MEMORY_SIZE: usize = 0x2000_0000;
-        pub const KERNEL_HEAP_SIZE: usize = 0x1000_0000;
+        pub const KERNEL_HEAP_SIZE: usize = 0x1200_0000;
         pub const PHYS_MEMORY_END: usize = PHYS_MEMORY_START + MEMORY_SIZE;
         pub const VALID_PHYS_CPU_MASK: usize = 0b1111;
     }
